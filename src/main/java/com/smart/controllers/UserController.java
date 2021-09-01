1b2f4a1bd47aa09f0acd81ffd9462ada63fd4893
Huy Quang <huyquang271099@gmail.com>
2021-09-01 23:51:46 +0700

    Working on User Dashboard

--- src/main/java/com/smart/controllers/UserController.java
+++ src/main/java/com/smart/controllers/UserController.java
@@ -1,13 +1,32 @@
 package com.smart.controllers;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.smart.entities.User;
+import com.smart.repository.UserRepository;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	
+	@Autowired
+	private UserRepository userRepo;
+	
 	@RequestMapping("/index")
-	public String dashboard() {
+	public String dashboard(Model model, Principal principal) {
+		String username = principal.getName();
+		System.out.println("USERNAME: " + username );
+		
+		User user = userRepo.getUserByUserName(username);
+		
+		System.out.println("Name: " + user);
+		model.addAttribute("user", user);
+		
 		return "normal/user_dashboard";
 	}
 }
