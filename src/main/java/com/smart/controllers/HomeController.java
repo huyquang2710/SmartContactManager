979cd18a5097eb526336a1b53e5173dfe0440be5
Huy Quang <huyquang271099@gmail.com>
2021-09-01 16:35:02 +0700

    Create project. Design Register. controller save

---
+++ src/main/java/com/smart/controllers/HomeController.java
@@ -0,0 +1,74 @@
+package com.smart.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.smart.entities.User;
+import com.smart.helper.Message;
+import com.smart.repository.UserRepository;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	private UserRepository userRepo;
+	
+	@RequestMapping("/")
+	public String home(Model model) {
+		model.addAttribute("title", "This is title");
+		return "home";
+	}
+	@RequestMapping("/about")
+	public String about(Model model) {
+		model.addAttribute("title", "This is about");
+		return "about";
+	}
+	@RequestMapping("/signup")
+	public String signup(Model model) {
+		model.addAttribute("title", "This is Signup");
+		model.addAttribute("user", new User());
+		return "signup";
+	}
+	
+//	handler for segistering user
+	@RequestMapping(value = "/do_register", method = RequestMethod.POST)
+	public String registerUser(
+			@ModelAttribute("user") User user,
+			@RequestParam(value = "agreement", defaultValue = "false") boolean agreement,
+			Model model,
+			HttpSession session ) {
+	
+		try {
+			if(!agreement) {
+				System.out.println("You have not agreed the terms and conditions!!!");
+				throw new Exception("You have not agreed the terms and conditions!!!");
+			}
+			user.setRole("ROLE_USER");
+			user.setEnabled(true);
+			user.setImageUrl("default.png");
+			
+			System.out.println("Agreement: " + agreement);
+			System.out.println("User: " + user);
+			
+			User result = userRepo.save(user);
+			model.addAttribute("user", new User());
+			
+			session.setAttribute("message", new Message("Successfully Registered!!", "alert-success"));
+			return "signup";
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("user", user);
+			session.setAttribute("message", new Message("Something went wrong!!" +e.getMessage(), "alert-danger"));
+			return  "signup";
+		}
+	
+	}
+	
+}
---
+++ src/main/java/com/smart/entities/Contact.java
@@ -0,0 +1,33 @@
+package com.smart.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor
+@Table(name = "contact")
+public class Contact {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String name;
+	private String secondName;
+	private String work;
+	private String email;
+	private String phone;
+	private String image;
+	@Column(length = 1000)
+	private String description;
+	
+	@ManyToOne
+	private User user;
+}
