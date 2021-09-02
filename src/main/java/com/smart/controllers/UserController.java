9bfd6acaebac3b4ebb66ec987f654dc2bbab387b
Huy Quang <huyquang271099@gmail.com>
2021-09-02 13:52:49 +0700

    process contact

--- src/main/java/com/smart/controllers/UserController.java
+++ src/main/java/com/smart/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.smart.entities.Contact;
@@ -44,6 +45,22 @@ public class UserController {
 		
 		model.addAttribute("title", "Add Contact");
 		model.addAttribute("contact", new Contact());
+		
+		return "normal/add_contact_form";
+	}
+	//process add contact form
+	@PostMapping("/process-contact")
+	public String processContact(@ModelAttribute Contact contact, Principal principal) {
+		
+		String name = principal.getName();
+		User user = this.userRepo.getUserByUserName(name);
+		contact.setUser(user);
+		user.getContacts().add(contact);
+		
+		this.userRepo.save(user);
+	
+		System.out.println("contact:" + contact);
+		System.out.println("Added to data");
 		
 		return "normal/add_contact_form";
 	}
--- src/main/java/com/smart/entities/User.java
+++ src/main/java/com/smart/entities/User.java
@@ -17,10 +17,12 @@ import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Data @AllArgsConstructor @NoArgsConstructor
+@Setter @Getter @AllArgsConstructor @NoArgsConstructor
 @Table(name = "user" )
 public class User {
 	
