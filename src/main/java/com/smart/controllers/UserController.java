fe76090e366730623b928df6c980202db9bbe2ef
Huy Quang <huyquang271099@gmail.com>
2021-09-02 14:16:16 +0700

    Uploading Image static/img

--- src/main/java/com/smart/controllers/UserController.java
+++ src/main/java/com/smart/controllers/UserController.java
@@ -1,14 +1,22 @@
 package com.smart.controllers;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.smart.entities.Contact;
 import com.smart.entities.User;
@@ -50,18 +58,40 @@ public class UserController {
 	}
 	//process add contact form
 	@PostMapping("/process-contact")
-	public String processContact(@ModelAttribute Contact contact, Principal principal) {
-		
+	public String processContact(
+			@ModelAttribute Contact contact,
+			@RequestParam("profileImage") MultipartFile file,
+			Principal principal) {
+		try {
 		String name = principal.getName();
 		User user = this.userRepo.getUserByUserName(name);
 		contact.setUser(user);
 		user.getContacts().add(contact);
 		
+		//upload image
+		if(file.isEmpty()) {
+			System.out.println("File is empty");
+		} else {
+			//file to folder and update name
+			contact.setImage(file.getOriginalFilename());
+			File saveFile = new ClassPathResource("static/img").getFile();
+			Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + file.getOriginalFilename());
+			
+			Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+			System.out.println("Upload success");
+		}
+		
 		this.userRepo.save(user);
+		
+		
 	
 		System.out.println("contact:" + contact);
-		System.out.println("Added to data");
-		
+		System.out.println("Added to data"); 
+		} catch(Exception e) {
+			System.out.println("Error: " + e.getMessage());
+			e.printStackTrace();
+		}
+ 		
 		return "normal/add_contact_form";
 	}
 }
